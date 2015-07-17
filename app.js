e93ea9c1d7406e2ac13f8eef5f23dd642b0c110a
endows <you@example.com>
2015-07-17 12:48:13 +0900

    tesu

--- app.js
+++ app.js
@@ -1,35 +1,5 @@
-var http = require('http');
-var fs = require('fs-extra')
-var targz = require('tar.gz');
-var zlib = require('zlib');
+var update = require('git-updater')
 
-onload = function() {
-  var webview = document.getElementById("foo");
-  webview.addEventListener("dom-ready", function() {
-    console.log(webview.getUrl())
-  });
-}
-
-extract = function(){
-  var compress = new targz().extract(__dirname + '/syng.tar.gz', __dirname, function(err){
-    fs.copy(__dirname + '/app', __dirname, function (err) {
-      if (err) return console.error(err)
-      fs.removeSync(__dirname + '/syng.tar.gz')
-      fs.removeSync(__dirname + '/app')
-    }) // copies directory, even if it has subdirectories or files
-  });
-}
-
-update = function(){
-
-  var file = fs.createWriteStream(__dirname + '/syng.tar.gz');
-  var request = http.get("http://localhost:8080/syng.tar.gz", function(response) {
-    var p = response.pipe(file)
-    p.on('end',function(){
-      extract()
-    })
-  });
-}
 
 
 update()
