0dc954048e1ea11bcff2490787649cd1226a91f4
endows <you@example.com>
2015-07-17 15:37:51 +0900

    tesu

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-var fs = require('fs')
-var request = require('request')
 
-// var file = fs.createWriteStream('/Applications/Syng.app/Contents/Resources/app.asar');
-update = function(url){
-  // request(url).pipe(file)
-  request(url,function(e,res,body){
-    if(res.statusCode == 200){
-      fs.writeFile('/Applications/Syng.app/Contents/Resources/app.asar.new',body,function(e){
-        fs.renameSync('/Applications/Syng.app/Contents/Resources/app.asar.new','/Applications/Syng.app/Contents/Resources/app.asar')
-      })
-    }
-  })
-}
